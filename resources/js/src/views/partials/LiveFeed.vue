c8b31606191ee9eae02d4ebae969d5b3eefd6c6b
Alexandre <a.elreco@gmail.com>
2020-03-15 19:20:16 +0100

    fix livefeed

--- resources/js/src/views/partials/LiveFeed.vue
+++ resources/js/src/views/partials/LiveFeed.vue
@@ -2,19 +2,22 @@
     <vx-card>
         <div class="vx-col w-full">
             <h4 class="mb-base">Live Feed</h4>
-            <vs-table max-items="3" pagination :data="users">
+            <vs-table
+            max-items="3"
+            pagination
+            v-model="selected"
+            :data="users" @selected="handleSelected">
 
                 <template slot="thead">
                     <vs-th>Email</vs-th>
                     <vs-th>Name</vs-th>
                     <vs-th>Website</vs-th>
                     <vs-th>Nro</vs-th>
-
+                    <vs-th>Nro</vs-th>
                 </template>
 
                 <template slot-scope="{data}">
-                    <vs-tr :key="indextr" v-for="(tr, indextr) in data">
-
+                    <vs-tr :data="tr" :key="indextr" v-for="(tr, indextr) in data">
                         <vs-td :data="data[indextr].email">
                             {{ data[indextr].email }}
                         </vs-td>
@@ -32,7 +35,6 @@
                         <vs-td :data="data[indextr].id">
                             {{ data[indextr].id }}
                         </vs-td>
-
                     </vs-tr>
                 </template>
             </vs-table>
@@ -42,33 +44,90 @@
 
 <script>
 export default {
-    name: 'live-feed',
-    data: () => ({
-        users: [
-            {
-                "id": 1,
-                "src": "http://ddragon.leagueoflegends.com/cdn/10.5.1/img/champion/Aatrox.png",
-                "name": "Leanne Graham",
-                "username": "Bret",
-                "email": "Sincere@april.biz",
-                "website": "hildegard.org",
-            }
-        ]
-    }),
-    methods:{
-    handleSelected(tr) {
-    this.$vs.notify({
-    title:`Selected ${tr.username}`,
-    text:`Email: ${tr.email}`
-    })
-    },
-    doubleSelection(tr) {
-    this.$vs.notify({
-    title:`Double Selection ${tr.username}`,
-    text:`Email: ${tr.email}`,
-    color: 'success'
-    })
+  data () {
+    return {
+      selected: [],
+      users: [
+        {
+          'id': 1,
+          'name': 'Leanne Graham',
+          'username': 'Bret',
+          'email': 'Sincere@april.biz',
+          'website': 'hildegard.org'
+        },
+        {
+          'id': 2,
+          'name': 'Ervin Howell',
+          'username': 'Antonette',
+          'email': 'Shanna@melissa.tv',
+          'website': 'anastasia.net'
+        },
+        {
+          'id': 3,
+          'name': 'Clementine Bauch',
+          'username': 'Samantha',
+          'email': 'Nathan@yesenia.net',
+          'website': 'ramiro.info'
+        },
+        {
+          'id': 4,
+          'name': 'Patricia Lebsack',
+          'username': 'Karianne',
+          'email': 'Julianne.OConner@kory.org',
+          'website': 'kale.biz'
+        },
+        {
+          'id': 5,
+          'name': 'Chelsey Dietrich',
+          'username': 'Kamren',
+          'email': 'Lucio_Hettinger@annie.ca',
+          'website': 'demarco.info'
+        },
+        {
+          'id': 6,
+          'name': 'Mrs. Dennis Schulist',
+          'username': 'Leopoldo_Corkery',
+          'email': 'Karley_Dach@jasper.info',
+          'website': 'ola.org'
+        },
+        {
+          'id': 7,
+          'name': 'Kurtis Weissnat',
+          'username': 'Elwyn.Skiles',
+          'email': 'Telly.Hoeger@billy.biz',
+          'website': 'elvis.io'
+        },
+        {
+          'id': 8,
+          'name': 'Nicholas Runolfsdottir V',
+          'username': 'Maxime_Nienow',
+          'email': 'Sherwood@rosamond.me',
+          'website': 'jacynthe.com'
+        },
+        {
+          'id': 9,
+          'name': 'Glenna Reichert',
+          'username': 'Delphine',
+          'email': 'Chaim_McDermott@dana.io',
+          'website': 'conrad.com'
+        },
+        {
+          'id': 10,
+          'name': 'Clementina DuBuque',
+          'username': 'Moriah.Stanton',
+          'email': 'Rey.Padberg@karina.biz',
+          'website': 'ambrose.net'
+        }
+      ]
     }
+  },
+  methods: {
+    handleSelected (tr) {
+      this.$vs.notify({
+        title: `Selected ${tr.username}`,
+        text: `Email: ${tr.email}`
+      })
     }
+  }
 }
 </script>
