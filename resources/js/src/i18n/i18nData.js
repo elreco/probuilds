1ba2631dee433f9c794bb604f666715f4ae3c44a
Alexandre LE CORRE <alexandre.le.corre@si2p.fr>
2020-03-16 18:48:11 +0100

    continue live feed

--- resources/js/src/i18n/i18nData.js
+++ resources/js/src/i18n/i18nData.js
@@ -1,20 +1,34 @@
 // en, de, fr, pt
 export default {
-  en: {
-      home: {
-          welcome: 'Welcome to Probuilds',
-          welcomeText: "League of Legends professional player's <strong class='text-warning'>builds</strong> across all regions"
-      }
-  },
-  de: {
-  },
-  fr: {
-     home: {
-         welcome: 'Bienvenue sur Probuilds',
-         welcomeText: "<strong class='text-warning'>Builds</strong> des joueurs professionnels sur League of Legends dans toutes les régions"
-     }
+    en: {
+        home: {
+            welcome: 'Welcome to Probuilds',
+            welcomeText: "League of Legends professional player's <strong class='text-warning'>builds</strong> across all regions"
+        },
+        LiveFeed: {
+            player: "Player",
+            vs: "VS.",
+            kda: 'KDA',
+            gold: 'Gold',
+            keystone: 'Keystone',
+            build: 'Build'
+        }
+    },
+    de: {},
+    fr: {
+        home: {
+            welcome: 'Bienvenue sur Probuilds',
+            welcomeText: "<strong class='text-warning'>Builds</strong> des joueurs professionnels sur League of Legends dans toutes les régions"
+        },
+        LiveFeed: {
+            player: "Joueur",
+            vs: "VS.",
+            kda: 'KDA',
+            gold: 'Gold',
+            keystone: 'Keystone',
+            build: 'Build'
+        }
 
-  },
-  pt: {
-  }
+    },
+    pt: {}
 }
--- resources/js/src/views/partials/LiveFeed.vue
+++ resources/js/src/views/partials/LiveFeed.vue
@@ -1,133 +1,131 @@
 <template>
-    <vx-card>
-        <div class="vx-col w-full">
-            <h4 class="mb-base">Live Feed</h4>
-            <vs-table
-            max-items="3"
-            pagination
-            v-model="selected"
-            :data="users" @selected="handleSelected">
+<vx-card>
+    <div class="vx-col w-full">
+        <h4 class="mb-base">Live Feed</h4>
+        <vs-table max-items="3" pagination v-model="selected" :data="users" @selected="handleSelected">
 
-                <template slot="thead">
-                    <vs-th>Email</vs-th>
-                    <vs-th>Name</vs-th>
-                    <vs-th>Website</vs-th>
-                    <vs-th>Nro</vs-th>
-                    <vs-th>Nro</vs-th>
-                </template>
+            <template slot="thead">
+                <vs-th></vs-th>
+                <vs-th></vs-th>
+                <vs-th>{{ $t("LiveFeed.player") }}</vs-th>
+                <vs-th>{{ $t("LiveFeed.vs") }}</vs-th>
+                <vs-th>{{ $t("LiveFeed.kda") }}</vs-th>
+                <vs-th>{{ $t("LiveFeed.gold") }}</vs-th>
+                <vs-th>{{ $t("LiveFeed.keystone") }}</vs-th>
+                <vs-th>{{ $t("LiveFeed.build") }}</vs-th>
+                <vs-th></vs-th>
+            </template>
 
-                <template slot-scope="{data}">
-                    <vs-tr :data="tr" :key="indextr" v-for="(tr, indextr) in data">
-                        <vs-td :data="data[indextr].email">
-                            {{ data[indextr].email }}
-                        </vs-td>
-                        <vs-td :data="data[indextr].src">
-                            <vs-avatar :src="data[indextr].src"/>
-                        </vs-td>
-                        <vs-td :data="data[indextr].username">
-                            {{ data[indextr].username }}
-                        </vs-td>
+            <template slot-scope="{data}">
+                <vs-tr :data="tr" :key="indextr" v-for="(tr, indextr) in data">
+                    <vs-td :data="data[indextr].date">
+                        {{ data[indextr].date }}
+                    </vs-td>
+                    <vs-td :data="data[indextr].champion">
+                        <vs-avatar :src="data[indextr].champion" />
+                    </vs-td>
+                    <vs-td :data="data[indextr].player">
+                        {{ data[indextr].player }}
+                    </vs-td>
+                    <vs-td :data="data[indextr].vs">
+                        <vs-avatar :src="data[indextr].vs" />
+                    </vs-td>
 
-                        <vs-td :data="data[indextr].id">
-                            {{ data[indextr].website }}
-                        </vs-td>
+                    <vs-td :data="data[indextr].kda">
+                        {{ data[indextr].kda }}
+                    </vs-td>
 
-                        <vs-td :data="data[indextr].id">
-                            {{ data[indextr].id }}
-                        </vs-td>
-                    </vs-tr>
-                </template>
-            </vs-table>
-        </div>
-    </vx-card>
+                    <vs-td :data="data[indextr].gold">
+                        {{ data[indextr].gold }}
+                    </vs-td>
+                    <vs-td :data="data[indextr].keystone">
+                        <vs-avatar :src="data[indextr].keystone" />
+                    </vs-td>
+                    <vs-td :data="data[indextr].slots">
+                        <vs-dropdown vs-custom-content v-for="slot in data[indextr].slots" :data="slot" :key="indexslot">
+                            <a class="flex items-center" href="#">
+                                <vs-avatar :src="slot" />
+                            </a>
+
+                            <vs-dropdown-menu>
+                                <h3 class="mb-0">Login</h3>
+                            </vs-dropdown-menu>
+                        </vs-dropdown>
+
+                    </vs-td>
+                    <vs-td :data="data[indextr].spells">
+                        <vs-avatar v-for="spell in data[indextr].spells" :data="spell" :key="indexspell" :src="spell" />
+                    </vs-td>
+                </vs-tr>
+            </template>
+        </vs-table>
+    </div>
+</vx-card>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      selected: [],
-      users: [
-        {
-          'id': 1,
-          'name': 'Leanne Graham',
-          'username': 'Bret',
-          'email': 'Sincere@april.biz',
-          'website': 'hildegard.org'
-        },
-        {
-          'id': 2,
-          'name': 'Ervin Howell',
-          'username': 'Antonette',
-          'email': 'Shanna@melissa.tv',
-          'website': 'anastasia.net'
-        },
-        {
-          'id': 3,
-          'name': 'Clementine Bauch',
-          'username': 'Samantha',
-          'email': 'Nathan@yesenia.net',
-          'website': 'ramiro.info'
-        },
-        {
-          'id': 4,
-          'name': 'Patricia Lebsack',
-          'username': 'Karianne',
-          'email': 'Julianne.OConner@kory.org',
-          'website': 'kale.biz'
-        },
-        {
-          'id': 5,
-          'name': 'Chelsey Dietrich',
-          'username': 'Kamren',
-          'email': 'Lucio_Hettinger@annie.ca',
-          'website': 'demarco.info'
-        },
-        {
-          'id': 6,
-          'name': 'Mrs. Dennis Schulist',
-          'username': 'Leopoldo_Corkery',
-          'email': 'Karley_Dach@jasper.info',
-          'website': 'ola.org'
-        },
-        {
-          'id': 7,
-          'name': 'Kurtis Weissnat',
-          'username': 'Elwyn.Skiles',
-          'email': 'Telly.Hoeger@billy.biz',
-          'website': 'elvis.io'
-        },
-        {
-          'id': 8,
-          'name': 'Nicholas Runolfsdottir V',
-          'username': 'Maxime_Nienow',
-          'email': 'Sherwood@rosamond.me',
-          'website': 'jacynthe.com'
-        },
-        {
-          'id': 9,
-          'name': 'Glenna Reichert',
-          'username': 'Delphine',
-          'email': 'Chaim_McDermott@dana.io',
-          'website': 'conrad.com'
-        },
-        {
-          'id': 10,
-          'name': 'Clementina DuBuque',
-          'username': 'Moriah.Stanton',
-          'email': 'Rey.Padberg@karina.biz',
-          'website': 'ambrose.net'
+    data() {
+        return {
+            selected: [],
+            users: [{
+                    'id': 1,
+                    'champion': 'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/champion/Senna.png',
+                    'date': 'Il y a 10 minutes',
+                    'player': 'Ruler',
+                    'vs': 'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/champion/Pyke.png',
+                    'kda': "7/8/22",
+                    'gold': '35k',
+                    'keystone': 'https://ddragon.leagueoflegends.com/cdn/img/perk-images/Styles/Inspiration/GlacialAugment/GlacialAugment.png',
+                    'subkeystone': 'https://ddragon.leagueoflegends.com/cdn/img/perk-images/Styles/7201_Precision.png',
+                    'slots': [
+                        'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/3864.png',
+                        'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/3179.png',
+                        'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/2055.png',
+                        'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/3814.png',
+                        '',
+                        '',
+                        '',
+                    ],
+                    'spells': [
+                        'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/spell/SummonerBarrier.png',
+                        'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/spell/SummonerFlash.png',
+                    ]
+                },
+                {
+                    'id': 2,
+                    'champion': 'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/champion/Pantheon.png',
+                    'date': 'Il y a 10 minutes',
+                    'player': 'Ruler',
+                    'vs': 'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/champion/Annie.png',
+                    'kda': "7/8/22",
+                    'gold': '35k',
+                    'keystone': 'https://ddragon.leagueoflegends.com/cdn/img/perk-images/Styles/Inspiration/GlacialAugment/GlacialAugment.png',
+                    'subkeystone': 'https://ddragon.leagueoflegends.com/cdn/img/perk-images/Styles/7201_Precision.png',
+                    'slots': [
+                        'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/3864.png',
+                        'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/3179.png',
+                        'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/2055.png',
+                        'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/3814.png',
+                        '',
+                        '',
+                        '',
+                    ],
+                    'spells': [
+                        'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/spell/SummonerHeal.png',
+                        'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/spell/SummonerFlash.png',
+                    ]
+                },
+            ]
         }
-      ]
+    },
+    methods: {
+        handleSelected(tr) {
+            this.$vs.notify({
+                title: `Selected ${tr.username}`,
+                text: `Email: ${tr.email}`
+            })
+        }
     }
-  },
-  methods: {
-    handleSelected (tr) {
-      this.$vs.notify({
-        title: `Selected ${tr.username}`,
-        text: `Email: ${tr.email}`
-      })
-    }
-  }
 }
 </script>
