387632ad867bb85cea545086eb192f59ab73be3f
Alexandre LE CORRE <alexandre.le.corre@si2p.fr>
2020-03-17 18:52:17 +0100

    ik

--- resources/js/src/views/partials/LiveFeed.vue
+++ resources/js/src/views/partials/LiveFeed.vue
@@ -51,10 +51,36 @@
                                 <h3>Test</h3>
                             </vs-dropdown-menu>
                         </vs-dropdown> -->
-                        <div class="inline" v-for="(slot, index) in data[indextr].slots" :key="index" :data="slot">
-                            <vs-avatar :src="slot" icon="fiber_manual_record" v-tooltip="'dfhd'" />
-                        </div>
+                        <div>
 
+                            <v-popover delay="300" container="body" trigger="hover" placement="auto" class="inline" v-for="(slot, index) in data[indextr].slots" :key="index" :data="slot">
+                                <vs-avatar class="tooltip-target" :src="slot.src" icon="fiber_manual_record" />
+                                <template slot="popover">
+                                    <vs-card class="cardx">
+                                        <div slot="header">
+                                            <h3>
+                                                Hello world !
+                                            </h3>
+                                        </div>
+                                        <div slot="media">
+                                            <img :src="src">
+                                        </div>
+                                        <div>
+                                            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                                                nisi ut aliquip ex ea commodo consequat.</span>
+                                        </div>
+                                        <div slot="footer">
+                                            <vs-row vs-justify="flex-end">
+                                                <vs-button type="gradient" color="danger" icon="favorite"></vs-button>
+                                                <vs-button color="primary" icon="turned_in_not"></vs-button>
+                                                <vs-button color="rgb(230,230,230)" color-text="rgb(50,50,50)" icon="settings"></vs-button>
+                                            </vs-row>
+                                        </div>
+                                    </vs-card>
+
+                                </template>
+                            </v-popover>
+                        </div>
                     </vs-td>
                     <vs-td :data="data[indextr].spells">
                         <vs-avatar v-for="(spell, index) in data[indextr].spells" :key="index" :data="spell" :src="spell" />
@@ -81,14 +107,30 @@ export default {
                     'gold': '35k',
                     'keystone': 'https://ddragon.leagueoflegends.com/cdn/img/perk-images/Styles/Inspiration/GlacialAugment/GlacialAugment.png',
                     'subkeystone': 'https://ddragon.leagueoflegends.com/cdn/img/perk-images/Styles/7201_Precision.png',
-                    'slots': [
-                        'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/3864.png',
-                        'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/3179.png',
-                        'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/2055.png',
-                        'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/3814.png',
-                        'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/3814.png',
-                        null,
-                        null,
+                    'slots': [{
+                            'src': 'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/3864.png',
+                            'id': 1,
+                        },
+                        {
+                            'src': 'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/3814.png',
+                            'id': 2,
+                        },
+                        {
+                            'src': 'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/3864.png',
+                            'id': 3,
+                        },
+                        {
+                            'src': 'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/2055.png',
+                            'id': 4,
+                        },
+                        {
+                            'src': 'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/3179.png',
+                            'id': 5,
+                        },
+                        {
+                            'src': null,
+                            'id': 1,
+                        }
                     ],
                     'spells': [
                         'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/spell/SummonerBarrier.png',
@@ -105,14 +147,30 @@ export default {
                     'gold': '35k',
                     'keystone': 'https://ddragon.leagueoflegends.com/cdn/img/perk-images/Styles/Inspiration/GlacialAugment/GlacialAugment.png',
                     'subkeystone': 'https://ddragon.leagueoflegends.com/cdn/img/perk-images/Styles/7201_Precision.png',
-                    'slots': [
-                        'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/3864.png',
-                        'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/3179.png',
-                        'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/2055.png',
-                        'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/3814.png',
-                        '',
-                        '',
-                        '',
+                    'slots': [{
+                            'src': 'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/3864.png',
+                            'id': 1,
+                        },
+                        {
+                            'src': 'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/3814.png',
+                            'id': 2,
+                        },
+                        {
+                            'src': 'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/3864.png',
+                            'id': 3,
+                        },
+                        {
+                            'src': 'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/2055.png',
+                            'id': 4,
+                        },
+                        {
+                            'src': 'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/3179.png',
+                            'id': 5,
+                        },
+                        {
+                            'src': null,
+                            'id': 1,
+                        }
                     ],
                     'spells': [
                         'https://ddragon.leagueoflegends.com/cdn/10.4.1/img/spell/SummonerHeal.png',
