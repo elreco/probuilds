e214824222e67567687abc8ba3f5dc0dfc7156ed
Alexandre LE CORRE <alexandre.le.corre@si2p.fr>
2020-06-08 15:08:37 +0200

    add none ban

--- config/app.php
+++ config/app.php
@@ -131,7 +131,7 @@ return [
     */
 
     // 'riot_api_key' => env('RIOT_API_KEY', "RGAPI-f09ce477-11c8-4651-992e-caee44fb1c99"),
-    'riot_api_key' => "RGAPI-ff20c39c-580d-4409-a626-b7f3659ced91",
+    'riot_api_key' => "RGAPI-dda61f3e-d972-48a0-ace3-c74ddbd484e0",
 
     /*
     |--------------------------------------------------------------------------
