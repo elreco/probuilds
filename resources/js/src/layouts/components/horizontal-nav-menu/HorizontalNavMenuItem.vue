93acdbd290019379ab83940d26275f6cf52fd0f0
Alexandre <a.elreco@gmail.com>
2020-07-03 16:21:41 +0200

    ok

--- resources/js/src/layouts/components/horizontal-nav-menu/HorizontalNavMenuItem.vue
+++ resources/js/src/layouts/components/horizontal-nav-menu/HorizontalNavMenuItem.vue
@@ -9,32 +9,40 @@
 ========================================================================================== -->
 
 <template>
-<div class="h-nav-menu-item" :class="[
+    <div
+        class="h-nav-menu-item"
+        :class="[
       {'h-nav-active-item text-primary font-medium' : activeLink},
       {'disabled-item pointer-events-none' : isDisabled},
-    ]">
+    ]"
+    >
+        <router-link
+            v-if="to"
+            exact
+            :class="[{'router-link-active': activeLink}, 'nav-link flex items-center']"
+            :to="to"
+            :target="target"
+        >
+            <vs-icon v-if="!featherIcon" :icon-pack="iconPack" :icon="icon" />
+            <feather-icon v-else :class="iconClasses" :icon="icon" />
+            <slot />
+        </router-link>
 
-    <router-link v-if="to" exact :class="[{'router-link-active': activeLink}, 'nav-link flex items-center']" :to="to" :target="target">
-        <vs-icon v-if="!featherIcon" :icon-pack="iconPack" :icon="icon" />
-        <feather-icon v-else :class="iconClasses" :icon="icon" />
-        <slot />
-    </router-link>
-
-    <a v-else :target="target" :href="href" class="nav-link flex items-center">
-        <vs-icon v-if="!featherIcon" :icon-pack="iconPack" :icon="icon" />
-        <feather-icon v-else :class="iconClasses" :icon="icon" />
-        <slot />
-    </a>
-</div>
+        <a v-else :target="target" :href="href" class="nav-link flex items-center">
+            <vs-icon v-if="!featherIcon" :icon-pack="iconPack" :icon="icon" />
+            <feather-icon v-else :class="iconClasses" :icon="icon" />
+            <slot />
+        </a>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'v-nav-menu-item',
+    name: "v-nav-menu-item",
     props: {
         icon: {
             type: String,
-            default: ''
+            default: ""
         },
         iconSmall: {
             type: Boolean,
@@ -42,11 +50,11 @@ export default {
         },
         iconPack: {
             type: String,
-            default: 'material-icons'
+            default: "material-icons"
         },
         href: {
             type: [String, null],
-            default: '#'
+            default: "#"
         },
         to: {
             type: [String, Object, null],
@@ -66,7 +74,7 @@ export default {
         },
         target: {
             type: String,
-            default: '_self'
+            default: "_self"
         },
         isDisabled: {
             type: Boolean,
@@ -75,13 +83,22 @@ export default {
     },
     computed: {
         iconClasses() {
-            return this.iconSmall ? 'w-3 h-3 mr-3' : 'w-5 h-5 mr-3'
+            return this.iconSmall ? "w-3 h-3 mr-3" : "w-5 h-5 mr-3";
         },
         activeLink() {
-            return !!((this.to === "/" + this.$route.params.lang + "/" + this.$route.name || this.to === this.$route.path || this.$route.meta.parent === this.slug) && this.to)
+            console.log(this.$route.matched[0].path);
+            console.log(this.to);
+            return !!(
+                (this.to ===
+                    "/" + this.$route.params.lang + "/" + this.$route.name ||
+                    this.to === this.$route.path ||
+                    this.to === this.$route.matched[1].name ||
+                    this.$route.meta.parent === this.slug) &&
+                this.to
+            );
         }
     }
-}
+};
 </script>
 
 <style lang="scss">
